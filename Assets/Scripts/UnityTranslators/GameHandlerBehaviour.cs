4102394d4ccdf9c91a3423e48326ee6ec0b38e93
Егор Безбородов <umenya2003@mail.ru>
2025-03-29 17:07:02 +0400

    Сприпт для обработки игры (почему то не работает)

---
+++ Assets/Scripts/UnityTranslators/GameHandlerBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+/// <summary>
+/// Менеджер, который отвечает за всю игровую сессию, её обновление и трансляцию в Unity
+/// </summary>
+public class GameHandlerBehaviour : MonoBehaviour
+{
+    private GameSession _session;
+
+    public List<TilePath> Paths { get ; set; }
+    public Tilemap WallTileMap { get; set; }
+    public Tilemap WalkableTileMap { get; set; }
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UnityTranslators/TilePath.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+/// <summary>
+/// Класс отвечает за реализацию поиска пути по заранее проложенным Tilemap'ам
+/// </summary>
+public class TilePath
+{
+    /// <summary>
+    /// Объект, из местоположения которого мобы будут спавнится
+    /// </summary>
+    [SerializeField]
+    public GameObject SpawnPoint { get; set; }
+    /// <summary>
+    /// Путь от спавнпоинта вперед
+    /// </summary>
+    [SerializeField]
+    public Tilemap Route { get; set;}
+}
+
